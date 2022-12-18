089062c47a6ef00831f384f1bd61f6b8af61c1ca
Gnesiuc <gnesiuc11998@gmail.com>
2022-12-18 21:00:15 +0200

    Все под копирку учителя(

--- More_CSS_JavaScript/More.js
+++ More_CSS_JavaScript/More.js
@@ -4,14 +4,11 @@ const login_acaund = document.querySelector(".login_acaund")
 const register_form = document.querySelector(".register-form")
 const login_form = document.querySelector(".login-form")
 
-create_acaund.addEventListener("click",()=>{
-    login_form.style.display = "none"
-    register_form.style.display ="block"
-})
-
-login_acaund.addEventListener("click",()=>{
-    register_form.style.display = "none"
-    login_form.style.display ="block"
-})
-
-//дом задание из этих двух функций сделать одну динамическую!
+  create_acaund.addEventListener("click",()=>{
+      login_form.style.display ="none"
+      register_form.style.display ="block"
+  })
+  login_acaund.addEventListener("click",()=>{
+      register_form.style.display ="none"
+      login_form.style.display ="block"
+  })
--- js/JavaScript.js
+++ js/JavaScript.js
@@ -1,15 +1,13 @@
 const header = document.querySelector("header")
 
-const header_bar_menu = ["Home", "News", "Sport", "More"]
+const header_bar_menu = ["Home","News","Sport","More"]
 
 
 
-const tag_ul = document.createElement("ul")  //создаем тег ul и даем ему класс
+const tag_ul = document.createElement("ul")
 tag_ul.className = "bar_list"
 
-
 header_bar_menu.forEach(item =>{
-
     const tag_li = document.createElement("li")
     tag_li.className = "item_li"
     const tag_a = document.createElement("a")
@@ -21,20 +19,24 @@ header_bar_menu.forEach(item =>{
     lastChild_tag("li","float_right item_li")
 })
 
-function  lastChild_tag  ( create_a_new_tag_element , className_tag ){
 
+function lastChild_tag( create_a_new_tag_element , className_tag){
     const tag_Selector = document.querySelectorAll(create_a_new_tag_element)
 
-
     tag_Selector.forEach(ele =>{
+  // ele.innerHTML.includes("More") ? ele.className = className_tag :null
+  if (ele.innerHTML.includes("More")) {
+    ele.className = className_tag
+}
+ })}
 
-        //ele.innerHtml.includes("More") ? ele.className = className_tag :null  более простой и удобный вариант 
-        
-        if (ele.innerHTML.includes("More")) {
 
-        ele.className = className_tag
-        }
-})}
+
+
+
+
+
+
 
 
 
@@ -42,39 +44,19 @@ const tag_a_All = document.querySelectorAll("a")
 
 const href_bar_menu = ["Navbar.html","News.html","Sport.html","More.html"]
 
-    // tag_a_All.forEach(element =>{
-    //     if (element.innerHTML.includes("Home")) {
-    //         element.href = "Navbar.html"
-    //     } else if(element.innerHTML.includes("News")){
-    //         element.href = "News.html"
-    //     } else if(element.innerHTML.includes("Sport")){
-    //         element.href = "Sport.html"
-    //     } else if(element.innerHTML.includes("More")){
-    //         element.href = "More.html"
-    //     } else{
-    //         element.href = "a"
-    //     }
-    // })
-
-    function href_page(tag_html , name_page , file_page)  {
-        tag_html.forEach(element =>{
-            console.log(element);
-            if (element.innerHTML.includes(name_page)) {
-                element.href = file_page 
-            } else {
+    tag_a_All.forEach(element =>{
+       
+            if (element.innerHTML.includes("Home")) {
+                element.href = "Navbar.html"
+            } else if(element.innerHTML.includes("News")){
+                element.href = "News.html"
+            } else if(element.innerHTML.includes("Sport")){
+                element.href = "Sport.html"
+            }else if(element.innerHTML.includes("More")){
+                element.href = "More.html"
+            }else{
                 element.href = "#"
-                
             }
-        })
-        
-    }
-
-href_page(tag_a_All , "News" , "News.html")
-href_page(tag_a_All , "Home" , "Home.html")
-href_page(tag_a_All , "Sport" , "Sport.html")
-
-
-
-
-
-
+    })
+   
+    
